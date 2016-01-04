07c720e276382eb0d7dba06fc3b22bdc676bda68
Akron <nils@diewald-online.de>
2016-01-04 20:56:46 +0100

    Added login form

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,6 +113,7 @@ module.exports = function(grunt) {
       css: {
 	files: ['dev/scss/{util,base,fonts,kalamar,media}.scss',
 		'dev/scss/footer/footer.scss',
+		'dev/scss/sidebar/sidebar.scss',
 		'dev/scss/header/{header,hint,menu,searchbar,vc,datepicker}.scss',
 		'dev/scss/main/{alertify,koralquery,highlight,kwic,logos,tagger,' +
 		'main,matchinfo,pagination,query,'+
