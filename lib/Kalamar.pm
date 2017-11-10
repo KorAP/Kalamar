41e7bddac5aa5bf06bd9dd19de32d8d9ce37d691
Akron <nils@diewald-online.de>
2017-11-10 16:28:35 +0100

    Update version
    Change-Id: I6eedfaf03826f7d3459b1c78f20615ab2da25cea

--- lib/Kalamar.pm
+++ lib/Kalamar.pm
@@ -7,7 +7,7 @@ use Mojo::Util qw/url_escape/;
 use File::Temp qw/tmpnam/;
 
 # Minor version - may be patched from package.json
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 # TODO: Use CSRF!!!
 # TODO: The FAQ-Page has a contact form for new questions
