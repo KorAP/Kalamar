39edba707446e36796e7aa0a2f22f57f9adf482e
Akron <nils@diewald-online.de>
2018-12-19 17:23:33 +0100

    Improve Piwik to track all requests anonymously
    Change-Id: I5c1d96c8f911e6208d8bc024ded799b1a9290c1d

--- lib/Kalamar/Plugin/Piwik.pm
+++ lib/Kalamar/Plugin/Piwik.pm
@@ -4,10 +4,28 @@ use Mojo::Base 'Mojolicious::Plugin';
 sub register {
   my ($plugin, $mojo, $param) = @_;
 
+  # Load parameter from Config file
+  if (my $config_param = $mojo->config('Kalamar')) {
+    if ($config_param->{Piwik}) {
+      $param = {
+        %$param,
+        %{$config_param->{Piwik}}
+      };
+    };
+  };
+
   # Load Piwik if not yet loaded
   unless (exists $mojo->renderer->helpers->{piwik_tag}) {
     $mojo->plugin('Piwik');
   };
+
+  # Add random string plugin
+  $mojo->plugin('Util::RandomString' => {
+    piwik_rand_id => {
+      alphabet => '0123456789abcdef',
+      length   => 16
+    }
+  });
 
   # Add opt-out to FAQ
   $mojo->content_block(
@@ -39,6 +57,39 @@ window.addEventListener('korapRequest', function(e) {
 % }
 SCRIPT
   });
+
+
+  # If all requests should be pinged,
+  # establish this hook
+  if ($param->{ping_requests}) {
+    $mojo->hook(
+      after_render => sub {
+        my $c = shift;
+
+        # Only track valid routes
+        my $route = $c->current_route or return;
+
+        # This won't forward personalized information
+        my $hash = {
+          action_url => $c->url_for->to_abs,
+          action_name => $route,
+          ua => '',
+          urlref => '',
+          send_image => 0,
+          dnt => 0,
+          uid => $c->random_string('piwik_rand_id')
+        };
+
+        # Overrid ping site id
+        if ($param->{ping_site_id}) {
+          $hash->{idsite} = $param->{ping_site_id}
+        };
+
+        # Send track
+        $c->piwik->api_p(Track => $hash)->wait;
+      }
+    );
+  };
 };
 
 
@@ -46,3 +97,13 @@ SCRIPT
 
 
 __END__
+
+# Parameters can be loaded from
+# {
+#   Kalamar => {
+#     Piwik => {
+#       ping_requests => 1,
+#       ping_site_id => 12
+#     }
+#   }
+# }
