13c3f225876474ba0d59499262b7a98757a5ac30
Akron <nils@diewald-online.de>
2019-07-23 11:35:00 +0200

    Implement token refresh mechanism
    Change-Id: Id58e14f663ebdd86f3f2206d4bfb9ad5d87a35fa

--- lib/Kalamar/Plugin/Auth.pm
+++ lib/Kalamar/Plugin/Auth.pm
@@ -3,8 +3,32 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::ByteStream 'b';
 
 # TODO:
+#   Establish a plugin 'OAuth' that works independent of 'Auth'.
+
+# TODO:
 #   CSRF-protect logout!
 
+# TODO:
+#   Remove the Bearer prefix from auth.
+
+# TODO:
+#   Never log tokens! (Not eben in debug)
+
+# TODO:
+#   The refresh token could be
+#   encrypted with a server-side secret.
+#   The session_id is stored in the cache,
+#   and has no timeout.
+#   But: The refresh token should be useless,
+#   as long as the user doesn't have the
+#   client_id/client_secret!
+
+# TODO:
+#   Define user CHI cache
+
+
+# In case no expiration time is returned by the server,
+# take this time.
 our $EXPECTED_EXPIRATION_IN = 259200;
 
 # Register the plugin
@@ -31,9 +55,6 @@ sub register {
     $app->log->error('client_id or client_secret not defined');
   };
 
-  # TODO:
-  #   Define user CHI cache
-
   $app->plugin('Localize' => {
     dict => {
       Auth => {
@@ -45,6 +66,8 @@ sub register {
           logoutFail => 'Abmeldung fehlgeschlagen',
           csrfFail => 'Fehlerhafter CSRF Token',
           openRedirectFail => 'Weiterleitungsfehler',
+          tokenExpired => 'Zugriffstoken abgelaufen',
+          tokenInvalid => 'Zugriffstoken ungültig',
           refreshFail => 'Fehlerhafter Refresh-Token'
         },
         -en => {
@@ -54,6 +77,8 @@ sub register {
           logoutFail => 'Logout failed',
           csrfFail => 'Bad CSRF token',
           openRedirectFail => 'Redirect failure',
+          tokenExpired => 'Access token expired',
+          tokenInvalid => 'Access token invalid',
           refreshFail => 'Bad refresh token'
         }
       }
@@ -76,54 +101,31 @@ sub register {
     }
   );
 
-  # Inject authorization to all korap requests
-  $app->hook(
-    before_korap_request => sub {
-      my ($c, $tx) = @_;
-      my $h = $tx->req->headers;
-
-      # If the request already has an Authorization
-      # header, respect it
-      unless ($h->authorization) {
-        my $auth_token = $c->auth->token or return;
-        $h->authorization($auth_token);
-
-      }
-
-      # TODO:
-      #   When a request fails because the access token timed out,
-      #   rerequest with the refresh token.
-
-      # TODO:
-      #   Check if the auth_token is timed out
-
-    }
-  );
-
 
   # Get or set the user token necessary for authorization
   $app->helper(
     'auth.token' => sub {
-      my ($c, $token) = @_;
+      my ($c, $token, $expires_in) = @_;
 
-      unless ($token) {
-        # Get token from stash
-        $token = $c->stash('auth');
-
-        return $token if $token;
-
-        # Get auth from session
-        $token = $c->session('auth') or return;
-
-        # Set token to stash
+      if ($token) {
+        # Set auth token
         $c->stash(auth => $token);
-
-        return $token;
+        $c->session(auth => $token);
+        $c->session(auth_exp => time + $expires_in);
+        return 1;
       };
 
-      # Set auth token
-      $c->stash('auth' => $token);
-      $c->session('auth' => $token);
+      # Get token from stash
+      $token = $c->stash('auth');
+
+      return $token if $token;
+
+      # Get auth from session
+      $token = $c->session('auth') or return;
+      $c->stash(auth => $token);
+
+      # Return stashed value
+      return $token;
     }
   );
 
@@ -136,38 +138,201 @@ sub register {
     my $client_id = $param->{client_id};
     my $client_secret = $param->{client_secret};
 
-    # This refreshes an oauth2 token and
-    # returns a promise
+
     $app->helper(
-      'auth.refresh_token' => sub {
+      'auth.refresh' => sub {
         my $c = shift;
         my $refresh_token = shift;
 
-        unless ($refresh_token) {
-          return Mojo::Promise->reject({message => 'Missing refresh token'})
-        };
-
         # Get OAuth access token
-        my $url = Mojo::URL->new($c->korap->api)->path('oauth2/token');
+        state $r_url = Mojo::URL->new($c->korap->api)->path('oauth2/token');
 
-        return $c->korap_request('POST', $url, {} => form => {
+        $c->app->log->debug("Refresh at " . $r_url);
+
+        return $c->kalamar_ua->post_p($r_url, {} => form => {
           grant_type => 'refresh_token',
           client_id => $client_id,
           client_secret => $client_secret,
           refresh_token => $refresh_token
-        })->then(
+        })
+      }
+    );
+
+    # Issue a korap request with "oauth"orization
+    # This will override the core request helper
+    $app->helper(
+      korap_request => sub {
+        my $c      = shift;
+        my $method = shift;
+        my $path   = shift;
+        my @param = @_;
+
+        # TODO:
+        #   Check if $tx is not leaked!
+
+        # Get plugin user agent
+        my $ua = $c->kalamar_ua;
+
+        my $url = Mojo::URL->new($path);
+        my $tx = $ua->build_tx(uc($method), $url->clone, @param);
+
+        # Set X-Forwarded for
+        $tx->req->headers->header(
+          'X-Forwarded-For' => $c->client_ip
+        );
+
+        # Emit Hook to alter request
+        $c->app->plugins->emit_hook(
+          before_korap_request => ($c, $tx)
+        );
+
+        my $h = $tx->req->headers;
+
+        # If the request already has an Authorization
+        # header, respect it!
+        if ($h->authorization) {
+          return $ua->start_p($tx);
+        };
+
+        # Get auth token
+        if (my $auth_token = $c->auth->token) {
+
+          # The token is already expired!
+          my $exp = $c->session('auth_exp');
+          if (defined $exp && $exp < time) {
+
+            # Remove auth ...
+            $c->stash(auth => undef);
+
+            # And get refresh token from session
+            if (my $refresh_token = $c->session('auth_r')) {
+
+              $c->app->log->debug("Refresh is required");
+
+              # This won't be tracked if before_korap_request is tracked, but who cares?
+              return $c->auth->refresh($refresh_token)->then(
+                sub {
+                  my $json = shift->result->json;
+                  $c->app->log->debug("Refresh was successful");
+
+                  # Set the tokens and return a promise
+                  return $plugin->_set_tokens(
+                    $c,
+                    $json
+                  )->then(
+                    sub {
+                      $c->app->log->debug("Search with refreshed tokens");
+
+                      # Tokens were set - now send the request the first time!
+                      $tx->req->headers->authorization($c->stash('auth'));
+                      return $ua->start_p($tx);
+                    }
+                  )
+                }
+              );
+            }
+
+            # The token is expired and no refresh token is
+            # available - issue an unauthorized request!
+            else {
+              $c->stash(auth => undef);
+              $c->stash(auth_exp => undef);
+              delete $c->session->{user};
+              delete $c->session->{auth};
+              delete $c->session->{auth_r};
+              delete $c->session->{auth_exp};
+
+              # Warn on Error!
+              $c->notify(warn => $c->loc('Auth_tokenExpired'));
+              return $ua->start_p($tx);
+            };
+          }
+
+          # Auth token is fine
+          else {
+
+            # Set auth
+            $h->authorization($auth_token);
+          }
+        }
+
+        # No token set
+        else {
+
+          # Return unauthorized request
+          return $ua->start_p($tx);
+        };
+
+        # Issue an authorized request and automatically
+        # refresh the token on expiration!
+        return $ua->start_p($tx)->then(
           sub {
-            # Set the tokens and return a promise
-            return $plugin->set_tokens(
-              $c,
-              shift->result->json
-            )
+            my $tx = shift;
+
+            # Response is fine
+            if ($tx->res->is_success) {
+              return Mojo::Promise->resolve($tx);
+            }
+
+            # There is a client error - maybe refresh!
+            elsif ($tx->res->is_client_error) {
+
+              # Check the error
+              my $json = $tx->res->json('/errors/0/1');
+              if ($json && ($json =~ /expired|invalid/)) {
+                $c->stash(auth => undef);
+                $c->stash(auth_exp => undef);
+                delete $c->session->{user};
+                delete $c->session->{auth};
+
+                # And get refresh token from session
+                if (my $refresh_token = $c->session('auth_r')) {
+
+                  return $c->auth->refresh($refresh_token)->then(
+                    sub {
+                      my $json = shift->result->json;
+                      $c->app->log->debug("Refresh was successful");
+
+                      # Set the tokens and return a promise
+                      return $plugin->_set_tokens(
+                        $c,
+                        $json
+                      )->then(
+                        sub {
+                          $c->app->log->debug("Search with refreshed tokens");
+
+                          my $tx = $ua->build_tx(uc($method), $url->clone, @param);
+
+                          # Set X-Forwarded for
+                          $tx->req->headers->header(
+                            'X-Forwarded-For' => $c->client_ip
+                          );
+
+                          # Tokens were set - now send the request the first time!
+                          $tx->req->headers->authorization($c->stash('auth'));
+                          return $ua->start_p($tx);
+                        }
+                      )
+                    }
+                  );
+                };
+
+                # Reject the invalid token
+                $c->notify(error => $c->loc('Auth_tokenInvalid'));
+                return Mojo::Promise->reject;
+              };
+
+              return Mojo::Promise->resolve($tx);
+            };
+
+            $c->notify(error => 'Unknown server error');
+            return Mojo::Promise->reject;
           }
         );
       }
     );
 
-    # Password flow
+    # Password flow for OAuth
     $r->post('/user/login')->to(
       cb => sub {
         my $c = shift;
@@ -201,7 +366,7 @@ sub register {
 
         my $pwd = $v->param('pwd');
 
-        $c->app->log->debug("Login from user $user:XXXX");
+        $c->app->log->debug("Login from user $user");
 
         # <specific>
 
@@ -218,7 +383,7 @@ sub register {
         })->then(
           sub {
             # Set the tokens and return a promise
-            return $plugin->set_tokens(
+            return $plugin->_set_tokens(
               $c,
               shift->result->json
             )
@@ -271,10 +436,41 @@ sub register {
         return 1;
       }
     )->name('login');
+
+
+    # Log out of the session
+    # $r->get('/user/logout')->to(
+    #  cb => sub {
+    #
+    #    # TODO!
+    #    return shift->redirect_to('index');
+    #  }
+    #)->name('logout');
   }
+
   # Use JWT login
+  # (should be deprecated)
   else {
 
+    # Inject authorization to all korap requests
+    $app->hook(
+      before_korap_request => sub {
+        my ($c, $tx) = @_;
+        my $h = $tx->req->headers;
+
+        # If the request already has an Authorization
+        # header, respect it
+        unless ($h->authorization) {
+
+          # Get valid auth token and set as header
+          if (my $auth_token = $c->auth->token) {
+            $h->authorization($auth_token);
+          };
+        };
+      }
+    );
+
+    # Password flow with JWT
     $r->post('/user/login')->to(
       cb => sub {
         my $c = shift;
@@ -308,7 +504,7 @@ sub register {
 
         my $pwd = $v->param('pwd');
 
-        $c->app->log->debug("Login from user $user:XXXX");
+        $c->app->log->debug("Login from user $user");
 
         my $url = Mojo::URL->new($c->korap->api)->path('auth/apiToken');
 
@@ -465,14 +661,16 @@ sub register {
 
 # Sets a requested token and returns
 # an error, if it didn't work
-sub set_tokens {
+sub _set_tokens {
   my ($plugin, $c, $json) = @_;
 
   my $promise = Mojo::Promise->new;
 
   # No json object
   unless ($json) {
-    return $promise->reject({message => 'Response is no valid Json object (remote)'});
+    return $promise->reject({
+      message => 'Response is no valid JSON object (remote)'
+    });
   };
 
   # There is an error here
@@ -483,7 +681,10 @@ sub set_tokens {
         message => $json->{error} . ($json->{error_description} ? ': ' . $json->{error_description} : '')
       }
     );
-  } elsif (my $error = $json->{errors} // $json->{error}) {
+  }
+
+  # There is an array of errors
+  elsif (my $error = $json->{errors} // $json->{error}) {
     if (ref $error eq 'ARRAY') {
       my @errors = ();
       foreach (@{$error}) {
@@ -497,24 +698,32 @@ sub set_tokens {
     return $promise->reject({message => $error});
   };
 
-  my $access_token = $json->{access_token};
-  my $token_type =  $json->{token_type};
+  # Everything is fine
+  my $access_token  = $json->{access_token};
+  my $token_type    =  $json->{token_type};
   my $refresh_token = $json->{refresh_token};
-  # my $scope = $json->{scope};
-  my $expires_in = $json->{"expires_in"} // $EXPECTED_EXPIRATION_IN;
-  my $auth = $token_type . ' ' . $access_token;
+  my $expires_in    = $json->{"expires_in"} // $EXPECTED_EXPIRATION_IN;
+  my $auth          = $token_type . ' ' . $access_token;
+  # my $scope       = $json->{scope};
 
   # Set session info
   $c->session(auth => $auth);
+
+  # Expiration of the token minus tolerance
+  $c->session(auth_exp => time + $expires_in - 60);
+
+  # Set session info for refresh token
+  # This can be stored in the session, as it is useless
+  # unless the client secret is stolen
+  $c->session(auth_r => $refresh_token) if $refresh_token;
 
   # Set stash info
   $c->stash(auth => $auth);
 
   # Remember refresh token in cache
-  $c->chi('user')->set(
-    "refr_" . $auth => $refresh_token,
-    $expires_in
-  );
+  # $c->chi('user')->set(
+  #   "refr_" . $auth => $refresh_token
+  # );
 
   return $promise->resolve;
 }
