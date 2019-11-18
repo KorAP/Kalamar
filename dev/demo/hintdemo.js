bfae327ef33eff5d742a6fbfb57d5d7409ef04fb
Akron <nils@diewald-online.de>
2019-11-18 19:49:50 +0100

    Added query panel for plugin capabilities
    Change-Id: I5766cf45aa711ebea9a6f9f3dfa51e706ebd0d0d

--- dev/demo/hintdemo.js
+++ dev/demo/hintdemo.js
@@ -7,11 +7,44 @@ requirejs.config({
 
 var hint = undefined;
 
-require(['hint','hint/foundries/cnx','lib/domReady'], function (hintClass, hintArray, domReady) {
+require(['plugin/server','panel/query', 'hint','hint/foundries/cnx','lib/domReady'], function (pluginClass, queryPanelClass, hintClass, hintArray, domReady) {
   KorAP.hintArray = hintArray;
   KorAP.Hint = null;
+  
   domReady(function() {
+
     KorAP.Hint = hintClass.create();
+
+    /**
+     * Add query panel
+     */
+    var queryPanel = queryPanelClass.create();
+
+    // Get input field
+    var sbar = document.getElementById("searchbar");
+    if (sbar) {
+      // The views are below the query bar
+      sbar.appendChild(queryPanel.element());
+      KorAP.Panel = KorAP.Panel || {};
+      KorAP.Panel['query'] = queryPanel;
+    }
+
+    // Load plugin server
+    KorAP.Plugin = pluginClass.create();
+
+    // Register match plugin
+    KorAP.Plugin.register({
+      'name' : 'Example New',
+      'desc' : 'Some content about cats',
+      'embed' : [{
+        'panel' : 'query',
+        'title' : 'Translate',
+        'classes' : ['translate'],
+        'onClick' : {
+          "template" : "http://localhost:3003/demo/plugin-client.html"
+        }
+      }]
+    });
   });
 });
 
