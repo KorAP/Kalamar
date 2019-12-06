e094dc674afb1dc4bb896cf258b0f626d4321fbe
hebasta <stallkamp@ids-mannheim.de>
2019-12-06 13:12:40 +0100

    Plugin Demo: result plugin added
    Change-Id: Iad77e5457cf1e72df55f1ce3059448d8a6635607

--- dev/demo/plugin-serverdemo.js
+++ dev/demo/plugin-serverdemo.js
@@ -5,16 +5,30 @@ requirejs.config({
   }
 });
 
-define(['app/en','match', 'panel/match', 'plugin/server','lib/domReady','init'], function (lang, matchClass, matchPanelClass, pluginClass, domReady) {
+
+
+
+define(['app/en','match', 'panel/match', 'panel/result', 'plugin/server','lib/domReady','init'], function (lang, matchClass, matchPanelClass, resultPanelClass, pluginClass, domReady) {
   domReady(function () {
-
-    // Initialize match
-    matchClass.create(
-      document.getElementById('WPD-FFF.01460-p119-120')
-    );
-
-    // Load plugin server
+ 
+    //Load Plugin Server first 
     KorAP.Plugin = pluginClass.create();
+    
+    //Register result plugin
+    KorAP.Plugin.register({
+       'name' : 'Export',
+       'desc' : 'Exports Kalamar results',
+       // 'about' : 'https://localhost:5678/',
+       'embed' : [{
+         'panel' : 'result',
+         'title' : 'Export',
+         'classes' : ['export'],
+         'onClick' : {
+           'action' : 'addWidget',
+           'template' : 'http://localhost:3003/demo/plugin-client.html',
+         }
+       }]
+     }); 
 
     // Register match plugin
     KorAP.Plugin.register({
@@ -31,6 +45,5 @@ define(['app/en','match', 'panel/match', 'plugin/server','lib/domReady','init'],
         }
       }]
     });
-
   });
 });
