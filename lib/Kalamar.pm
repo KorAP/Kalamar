58f7c815070dfc1f606d3631fc7d96bb28878006
Akron <nils@diewald-online.de>
2020-10-07 22:44:34 +0200

    Bump version
    Change-Id: Ia325264a5e11cb737a7507551c342ed61a29edbd

--- lib/Kalamar.pm
+++ lib/Kalamar.pm
@@ -8,7 +8,7 @@ use Mojo::Util qw/url_escape deprecated slugify/;
 use List::Util 'none';
 
 # Minor version - may be patched from package.json
-our $VERSION = '0.39';
+our $VERSION = '0.40';
 
 # Supported version of Backend API
 our $API_VERSION = '1.0';
