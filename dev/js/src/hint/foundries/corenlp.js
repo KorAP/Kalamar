08dea79ed6f44abfd59cef8a727175278359cac6
Akron <nils@diewald-online.de>
2020-10-09 08:52:50 +0200

    Modernize ES
    Change-Id: I1aab691d5b7e8167b6213378bdd9139c133202cd

--- dev/js/src/hint/foundries/corenlp.js
+++ dev/js/src/hint/foundries/corenlp.js
@@ -1,31 +1,33 @@
-define(["hint/foundries","hint/foundries/stts","hint/foundries/negranodes","hint/foundries/negraedges"],
-       function (ah, sttsArray, negraNodesArray, negraEdgesArray) {
-  var namedEntities = [
-    ["I-LOC",  "I-LOC ",  "Location"],
-    ["I-MISC", "I-MISC ", "Miscellaneous"],
-    ["I-ORG",  "I-ORG ",  "Organization"],
-    ["I-PER",  "I-PER ",  "Person"]
-  ];
+define(
+  ["hint/foundries","hint/foundries/stts","hint/foundries/negranodes","hint/foundries/negraedges"],
+  function (ah, sttsArray, negraNodesArray, negraEdgesArray) {
+    var namedEntities = [
+      ["I-LOC",  "I-LOC ",  "Location"],
+      ["I-MISC", "I-MISC ", "Miscellaneous"],
+      ["I-ORG",  "I-ORG ",  "Organization"],
+      ["I-PER",  "I-PER ",  "Person"]
+    ];
 
-  ah["-"].push(
-    ["CoreNLP", "corenlp/", "Constituency, Named Entities, Part-of-Speech"]
-  );
+    ah["-"].push(
+      ["CoreNLP", "corenlp/", "Constituency, Named Entities, Part-of-Speech"]
+    );
 
-  ah["corenlp/"] = [
-    ["Constituency", "c="],
-    ["Named Entity", "ne=" , "Combined"],
-    ["Named Entity", "ne_dewac_175m_600=" , "ne_dewac_175m_600"],
-    ["Named Entity", "ne_hgc_175m_600=",    "ne_hgc_175m_600"],
-    ["Part-of-Speech", "p="]
-  ];
+    ah["corenlp/"] = [
+      ["Constituency", "c="],
+      ["Named Entity", "ne=" , "Combined"],
+      ["Named Entity", "ne_dewac_175m_600=" , "ne_dewac_175m_600"],
+      ["Named Entity", "ne_hgc_175m_600=",    "ne_hgc_175m_600"],
+      ["Part-of-Speech", "p="]
+    ];
 
-  ah["corenlp/ne="] = namedEntities;
-  ah["corenlp/ne_dewac_175m_600="] = namedEntities;
-  ah["corenlp/ne_hgc_175m_600="] = namedEntities;
-  ah["corenlp/p="] = sttsArray;
-  ah["corenlp/c="] = negraNodesArray;
+    ah["corenlp/ne="] = namedEntities;
+    ah["corenlp/ne_dewac_175m_600="] = namedEntities;
+    ah["corenlp/ne_hgc_175m_600="] = namedEntities;
+    ah["corenlp/p="] = sttsArray;
+    ah["corenlp/c="] = negraNodesArray;
 
-  for (var i in negraNodesArray) {
-    ah["corenlp/c=" + negraNodesArray[i][0] + '-'] = negraEdgesArray;
-  };
-});
+    negraNodesArray.forEach(
+      i => ah["corenlp/c=" + i[0] + '-'] = negraEdgesArray
+    );
+  }
+);
