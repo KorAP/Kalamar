a283365c6f63e421edfd0782eba00ba71a8083f4
Akron <nils@diewald-online.de>
2021-01-26 18:01:04 +0100

    Make notifications framework CSP compliant (WIP)
    Change-Id: I95411f646053d76219908b91e9f0921c17280c28

--- dev/js/src/init.js
+++ dev/js/src/init.js
@@ -95,7 +95,7 @@ define([
   KorAP.tourshowR = function(){
     tourClass.gTshowResults().start();
   };
-    
+
   domReady(function (event) {
       
     var obj = {};
@@ -108,15 +108,19 @@ define([
     /**
      * Release notifications
      */
-    if (KorAP.Notifications !== undefined) {
-      KorAP.Notifications.forEach(function(n) {
-        var msg = n[1];
-        if (n[2]) {
-          msg += '<code class="src">'+n[2]+'</code>';
+    d.querySelectorAll('#notifications div.notify').forEach(
+      function(e) {
+        let msg = e.textContent;
+
+        let src = e.getAttribute('data-src');
+        if (src) {
+          msg += '<code class="src">'+src+'</code>';
         };
-        alertifyClass.log(msg, n[0], 10000);
-      });
-    };
+
+        let type = e.getAttribute('data-type') || "error";
+        alertifyClass.log(msg, type, 10000);
+      }
+    );
 
     /**
      * Replace Virtual Corpus field
