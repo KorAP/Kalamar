0df2843386cc1047a95ac7122c7224e1e5c68365
Leo Repp <Leo-Aus-Berlin@korap.ids-mannheim.de>
2021-04-26 15:53:22 +0200

    New menu class that has an entry at the very end, similar to the input text prefix, that is always available
    Change-Id: I03d8f689e37021d3daac6bf34f7d35f0e4d71999

---
+++ dev/js/src/alwaysentry.js
@@ -0,0 +1,42 @@
+/**
+ * An entry in menus that is always
+ * displayed, with a string and onClick functionality
+ * uses menu/prefix.js as prototype and doesn't change much, all
+ * functionality comes from the alwaysmenu
+ * 
+ * 
+ * @author Leo Repp
+ */
+
+
+"use strict";
+
+define([
+  'menu/prefix' //TODO How does this work Nils?
+], function (prefixClass) {
+  
+  return {
+
+    /**
+     * Create new always visible menu entry object.
+     * Like a prefix Object, always visible, for some defined action
+     */
+    create : function () {
+      const obj = prefixClass.create()
+        .upgradeTo(this)
+        ._init();
+      obj._el.innerHTML="Speichern";
+      return obj;
+    },
+
+    _update : function () {
+      if (this._string.length!==0){ // I assume that this is a sufficient criterium for infering that the prefix is active
+        this._el.style.bottom="-27px";
+      } else if (this._string.length===0) {
+        this._el.style.bottom="0px";
+      }
+      return this._string; // No need to change the text (=innerHTML)
+    },
+
+  };
+});
