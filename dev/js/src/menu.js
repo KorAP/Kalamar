7bf8b3c3ddb7ee12463dbc95a4240dd531943cd4
Leo Repp <Leo-Aus-Berlin@korap.ids-mannheim.de>
2021-06-30 13:51:07 +0200

    New menu with a container for mutliple always available non scrollable items
    Change-Id: I5c8379cc82038da4a6c0923bbf59ec8faaa1eb9f

--- dev/js/src/menu.js
+++ dev/js/src/menu.js
@@ -55,7 +55,6 @@ define([
 
     // Initialize list
     _init : function (list, params) {
-      
       if (params === undefined)
         params = {};
 
@@ -622,10 +621,15 @@ define([
      * Delete all visible items from the menu element
      */
     
-     removeItems : function () {
+    removeItems : function () {
       const liElements=this._el.getElementsByTagName("LI");
-      while (liElements.length>0){
-        this._el.removeChild(liElements[0]);
+      var ignoredCount = 0; //counts how many LI tag elements are not actually direct children
+      while (liElements.length>ignoredCount){
+        if (liElements[ignoredCount].parentNode === this._el){
+          this._el.removeChild(liElements[ignoredCount]);
+        } else {
+          ignoredCount++;
+        }
       };
      },
       
@@ -878,6 +882,7 @@ define([
 
         // Remove the HTML node from the first item
         // leave lengthField/prefix/slider
+        //console.log("_showItems, at _notItemElements is: ",t._el.children[this._notItemElements]);
         t._el.removeChild(t._el.children[this._notItemElements]);
 
         t._append(
@@ -890,6 +895,7 @@ define([
         t.offset = off;
 
         // Remove the HTML node from the last item
+        //console.log("_showItems, at lastChild is: ",t._el.lastChild);
         t._el.removeChild(t._el.lastChild);
 
         t._prepend(t._list[t.offset]);
