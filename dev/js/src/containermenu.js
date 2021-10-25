767701bde634b9b60c06445c1584cf0e8c5a5c3c
Leo Repp <Leo-Aus-Berlin@korap.ids-mannheim.de>
2021-10-25 11:05:32 +0200

    Fixed an error in containermenu
    Change-Id: Iebdb778629a85cdb77aa7f4a6982dba25484bff5

--- dev/js/src/containermenu.js
+++ dev/js/src/containermenu.js
@@ -165,7 +165,7 @@ define([
      * @param {string} Prefix for filtering the list
      */
     show : function (active) {
-      //there are only two new lines, marked with NEW
+      //there are only four new lines, marked with NEW
       const t = this;
 
       // show menu based on initial offset
@@ -177,11 +177,12 @@ define([
       if (!t._initList()) {
 
         // The prefix is not active
-        t._prefix.active(true);
+        t._prefix.active(true);        
+        t.container().makeActive(); //NEW incase the own list becomes empty we need to make container active for line 129
 
         // finally show the element
-        t._el.classList.add('visible'); // TODO do I need this for container?
-        t.container()._el.classList.add('visible');
+        t._el.classList.add('visible');
+        t.container()._el.classList.add('visible'); //NEW
 
         return true;
       };
