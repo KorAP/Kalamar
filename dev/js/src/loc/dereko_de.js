6c21cac51d9bca235c05a11c2d751956435e72c2
hebasta <stallkamp@ids-mannheim.de>
2021-11-02 19:56:01 +0100

    Tour: extend explanations to vc creation (fixes #147)
    Change-Id: I50e41b9cb0d26082d1cdf9de6ad4571430f4ccd2

---
+++ dev/js/src/loc/dereko_de.js
@@ -0,0 +1,13 @@
+/**
+ * Corpus specific German localization, here for DeReKo (http://www1.ids-mannheim.de/kl/projekte/korpora.html)
+ * 
+ * @author Helge Stallkamp
+ */
+"use strict";
+
+define(function () {
+  const loc = KorAP.Locale;
+  
+  loc.TOUR_vccho4 = "Ein Beispiel: Bilden eines virtuellen Korpus, der alle Texte mit der Dokumentsigle WPD17/D01 enthält.";
+
+});
