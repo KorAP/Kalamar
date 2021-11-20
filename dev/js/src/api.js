2c2d0b3a3aed1298d7867fb309237e350a3175d7
Leo Repp <Leo-Aus-Berlin@korap.ids-mannheim.de>
2021-11-20 18:45:17 +0100

    Demo for how to implement Querystoring.
    Change-Id: I6ce57e591860dd11d7746eeee9e83ac25f3896ff

--- dev/js/src/api.js
+++ dev/js/src/api.js
@@ -166,6 +166,7 @@ define(['util'], function () {
             KorAP.log(0, e);
             console.log(e);
             returnValueCB(undefined);
+            //errorCB({"status" : 0, "statusText": e + " - Could not parse returned Object."}); // Is this required???
             return;
           };
 
@@ -194,9 +195,26 @@ define(['util'], function () {
         };
         // Call the callback function (no matter requestType) if one is given.
         if (typeof(errorCB) === "function"){
+          var statusTextErrors = ""; // For some reason, the errors created in QueryReference.pm have their text stored in this.responseText.
+          // Here we try to extract this information
+          try {
+              JSON.parse(this.responseText).errors.forEach(
+                e => statusTextErrors = statusTextErrors + e["message"] || ""
+              );
+          } catch {
+            try {
+              if (requestType !== "GET"){
+                statusTextErrors += JSON.parse(this.responseText);
+              }
+            } catch {
+              //Nothing
+            }
+          }
           errorCB({
             "status" : this.status,
-            "statusText" : this.statusText
+            "statusText" : this.statusText + " - " + (statusTextErrors || "")
+            //responseText: A DOMString which contains either the textual data received using the 
+            // XMLHttpRequest or null if the request failed or "" if the request has not yet been sent by calling send(). 
           });
         }; 
       };
@@ -297,6 +315,7 @@ define(['util'], function () {
 
   /**
    * Post new query by query name
+   * CAUTION: Currently not supported by the QueryReference Plugin.
    * 
    * @param {String} qn The name of the new query
    * @param {JSObj} jsObj The query. This will be stringified
