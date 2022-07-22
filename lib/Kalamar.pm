b7749058abe77351f2b499be0dc16aa3b6bf159c
Akron <nils@diewald-online.de>
2022-07-22 18:03:26 +0200

    Improve token filtering in leaf nodes (fixes #168)
    Change-Id: Ia53ebbe28c1f4ed861a48159c6bc88ad7dece3de

--- lib/Kalamar.pm
+++ lib/Kalamar.pm
@@ -8,7 +8,7 @@ use Mojo::Util qw/url_escape deprecated slugify/;
 use List::Util 'none';
 
 # Minor version - may be patched from package.json
-our $VERSION = '0.45';
+our $VERSION = '0.46';
 
 # Supported version of Backend API
 our $API_VERSION = '1.0';
