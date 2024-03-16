8e79e246c3cd7ef6d7d82268a52b38f7b4824f5b
Marc Kupietz <kupietz@ids-mannheim.de>
2024-03-16 21:06:38 +0100

    Extract UPOS tags in annotation helper
    Change-Id: Ib04f7d853e998ec38443ac3b8abe1484b111c05a

---
+++ dev/js/src/hint/foundries/upos.js
@@ -0,0 +1,23 @@
+define(function () {
+  return [
+    // Universal POS tags
+    // https://universaldependencies.org/u/pos/
+    ["ADJ","ADJ ","adjective"],
+    ["ADP","ADP ","adposition"],
+    ["ADV","ADV ","adverb"],
+    ["AUX","AUX ","auxiliary"],
+    ["CCONJ","CCONJ ","coordinating conjunction"],
+    ["DET","DET ","determiner"],
+    ["INTJ","INTJ ","interjection"],
+    ["NOUN","NOUN ","noun"],
+    ["NUM","NUM ","numeral"],
+    ["PART","PART ","particle"],
+    ["PRON","PRON ","pronoun"],
+    ["PROPN","PROPN ","proper noun"],
+    ["PUNCT","PUNCT ","punctuation"],
+    ["SCONJ","SCONJ ","subordinating conjunction"],
+    ["SYM","SYM ","symbol"],
+    ["VERB","VERB ","verb"],
+    ["X","X ","other"]
+  ];
+});
